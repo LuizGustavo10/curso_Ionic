4d6aa65e22057391d6eeae91a4dc33210bbabb47
Luiz Gustavo <gustavo180g@gmail.com>
2020-01-17 10:11:06 -0300

    Aprendendo sobre slide

--- CursoIonic4/src/app/app-routing.module.ts
+++ CursoIonic4/src/app/app-routing.module.ts
@@ -43,7 +43,8 @@ const routes: Routes = [
   { path: 'ComponentModal', loadChildren: './component-modal/component-modal.module#ComponentModalPageModule' },
   { path: 'ComponentModalInterno', loadChildren: './component-modal-interno/component-modal-interno.module#ComponentModalInternoPageModule' },
   { path: 'ComponentRange', loadChildren: './component-range/component-range.module#ComponentRangePageModule' },
-  { path: 'ComponentSelect', loadChildren: './component-select/component-select.module#ComponentSelectPageModule' }
+  { path: 'ComponentSelect', loadChildren: './component-select/component-select.module#ComponentSelectPageModule' },
+  { path: 'ComponentSlide', loadChildren: './component-slide/component-slide.module#ComponentSlidePageModule' }
   
 ];
 
